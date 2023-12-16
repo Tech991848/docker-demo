cddd3f0c71bae070243fe80cbfe2eddabf10a386
dilip <dilip.tech.developer@gmail.com>
2023-12-16 22:30:48 +0530

    Finst commit

---
+++ src/main/java/com/dilip/dockeerjenkinsitegrationdemo/DockeerJenkinsItegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dilip.dockeerjenkinsitegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockeerJenkinsItegrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockeerJenkinsItegrationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dilip/dockeerjenkinsitegrationdemo/DockeerJenkinsItegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilip.dockeerjenkinsitegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockeerJenkinsItegrationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
